d418447525123ecc1849d13bd4e284dd9b89959a
millz60 <Matt@matts-macbook-pro.local>
2016-06-16 19:18:57 -0500

    Initial Commit

---
+++ Thermostat2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thermostat2
+//
+//  Created by Matt Milner on 6/16/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
